46bd9a0ff07e8ac3579730f764481a7ce2d5677e
Basit Warsi <basitwarsi2595@gmail.com>
2025-05-22 21:14:11 +0530

    media queries

--- src/ai.js
+++ src/ai.js
@@ -1,7 +1,17 @@
 import { HfInference } from '@huggingface/inference'
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page such that steps are in form of an odered list. Include a title for the recipe, a list of ingredients, and the steps to make the recipe. The ingredients should be in a bulleted list and the steps should be in an ordered list. Make sure to include the cooking time and serving size at the top of the recipe. If you don't know how to make a recipe with the ingredients provided, say "I don't know how to make a recipe with those ingredients."
+You are an assistant that receives a list of ingredients that a user has 
+and suggests a recipe they could make with some or all of those ingredients. 
+You don't need to use every ingredient they mention in your recipe. 
+The recipe can include additional ingredients they didn't mention, 
+but try not to include too many extra ingredients. 
+Format your response in markdown to make it easier to render
+ to a web page such that steps are in form of an odered list. 
+ Include a title for the recipe, a list of ingredients, and the 
+ steps to make the recipe. The ingredients should be in a bulleted
+  list and the steps should be in an ordered list. Make sure to include
+   the cooking time and serving size at the top of the recipe."
 `
 
 const hf = new HfInference(import.meta.env.VITE_HUGGINGFACE_API_KEY)
